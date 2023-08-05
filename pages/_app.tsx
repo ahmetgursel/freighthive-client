036a6373e9ddea3600da48acd825e8012a3ab994
ahmetgursel <ahmetgursel@protonmail.com>
2023-08-05 04:04:33 +0300

    Update page title to "FreightHive" in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import NextApp, { AppProps, AppContext } from 'next/app';
-import { getCookie, setCookie } from 'cookies-next';
-import Head from 'next/head';
-import { MantineProvider, ColorScheme, ColorSchemeProvider } from '@mantine/core';
+import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
+import { getCookie, setCookie } from 'cookies-next';
+import NextApp, { AppContext, AppProps } from 'next/app';
+import Head from 'next/head';
+import { useState } from 'react';
 
 export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   const { Component, pageProps } = props;
@@ -18,7 +18,7 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   return (
     <>
       <Head>
-        <title>Mantine next example</title>
+        <title>FreightHive</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
