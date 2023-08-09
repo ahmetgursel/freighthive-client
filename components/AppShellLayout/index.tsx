b3c47aec1b9c006aebfd78b6868af8b28f14eb2f
ahmetgursel <ahmetgursel@protonmail.com>
2023-08-09 03:08:29 +0300

    Update background color logic in AppShellLayout

--- components/AppShellLayout/index.tsx
+++ components/AppShellLayout/index.tsx
@@ -94,8 +94,7 @@ const AppShellLayout: React.FC<AppShellLayoutProps> = ({ children }) => {
       }
       styles={(theme) => ({
         main: {
-          backgroundColor:
-            theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+          backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.white,
         },
       })}
     >
