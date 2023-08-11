a085232fa4b60a6a679565ee2cbebb022a04b71c
ahmetgursel <ahmetgursel@protonmail.com>
2023-08-12 02:43:54 +0300

    Add organizations API endpoint with GET and POST methods

---
+++ pages/api/organizations/index.ts
@@ -0,0 +1,37 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import nookies from 'nookies';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    try {
+      const cookies = nookies.get({ req });
+      const accessToken = cookies.access_token;
+
+      if (!accessToken) {
+        res.status(401).json({ error: 'Access token not found' });
+        return;
+      }
+
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/organizations`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch organizations data');
+      }
+
+      const organizationsData = await response.json();
+      res.status(200).json(organizationsData);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred' });
+    }
+  } else if (req.method === 'POST') {
+    res.status(200).json({ message: 'POST method' });
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}
