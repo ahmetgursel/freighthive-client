8f0f975502cdb397e32873529f0c76cc04ff80e2
ahmetgursel <ahmetgursel@protonmail.com>
2023-08-11 04:04:22 +0300

    Add API endpoint for handling GET and POST requests to retrieve and create trucks

---
+++ pages/api/trucks/index.ts
@@ -0,0 +1,39 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import nookies from 'nookies';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    try {
+      const cookies = nookies.get({ req });
+      const accessToken = cookies.access_token;
+
+      if (!accessToken) {
+        res.status(401).json({ error: 'Access token not found' });
+        return;
+      }
+
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/trucks`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch trucks data');
+      }
+
+      const trucksData = await response.json();
+
+      res.status(200).json(trucksData);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred' });
+    }
+  } else if (req.method === 'POST') {
+    const newTruckData = req.body;
+    res.status(201).json({ message: 'Truck created successfully' });
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
+  }
+}
