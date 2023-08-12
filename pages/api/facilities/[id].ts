0f193148bde990fda5fec0cd4807ba9d65f2794b
ahmetgursel <ahmetgursel@protonmail.com>
2023-08-12 04:09:23 +0300

    Add API endpoint for getting facility details by ID

---
+++ pages/api/facilities/[id].ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import nookies from 'nookies';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const {
+    query: { id },
+    method,
+  } = req;
+
+  if (method === 'GET') {
+    try {
+      const cookies = nookies.get({ req });
+      const accessToken = cookies.access_token;
+
+      if (!accessToken) {
+        res.status(401).json({ error: 'Access token not found' });
+        return;
+      }
+
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/facility/${id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch facility data');
+      }
+
+      const facilityDetails = await response.json();
+      res.status(200).json(facilityDetails);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}
