e1aee209da06ae309670c4a30fbf159246ebdd98
ahmetgursel <ahmetgursel@protonmail.com>
2023-08-12 03:49:52 +0300

    Update PATCH method to handle organization data updates

--- pages/api/organizations/[id].ts
+++ pages/api/organizations/[id].ts
@@ -35,7 +35,35 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       res.status(500).json({ error: 'An error occurred' });
     }
   } else if (method === 'PATCH') {
-    res.status(200).json({ message: { id, method } });
+    try {
+      const cookies = nookies.get({ req });
+      const accessToken = cookies.access_token;
+
+      if (!accessToken) {
+        res.status(401).json({ error: 'Access token not found' });
+      }
+
+      const updatedOrganizationData = req.body;
+
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/organizations/${id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify(updatedOrganizationData),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to update organization data');
+      }
+
+      const updatedOrganization = await response.json();
+
+      res.status(200).json(updatedOrganization);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred' });
+    }
   } else if (method === 'DELETE') {
     res.status(200).json({ message: { id, method } });
   } else {
