fb062629452b8cb845dad1264885f8885bfbba19
ahmetgursel <ahmetgursel@protonmail.com>
2023-08-12 04:04:45 +0300

    Add facility creation endpoint in API

--- pages/api/facilities/index.ts
+++ pages/api/facilities/index.ts
@@ -31,6 +31,35 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       res.status(500).json({ error: 'An error occurred' });
     }
   } else if (req.method === 'POST') {
+    try {
+      const cookies = nookies.get({ req });
+      const accessToken = cookies.access_token;
+
+      if (!accessToken) {
+        res.status(401).json({ error: 'Access token not found' });
+        return;
+      }
+
+      const newFacilityData = req.body;
+
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/facility`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify(newFacilityData),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to create a facility');
+      }
+
+      const createdTruckData = await response.json();
+      res.status(201).json(createdTruckData);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred' });
+    }
   } else {
     res.status(405).json({ error: 'Method not allowed' });
   }
