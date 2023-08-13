7584a6b849ac5604c536c57771dcae41efe0fb95
ahmetgursel <ahmetgursel@protonmail.com>
2023-08-13 23:31:07 +0300

    Add API endpoint for getting tickets data

---
+++ pages/api/tickets/index.ts
@@ -0,0 +1,36 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import nookies from 'nookies';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    try {
+      const cookies = nookies.get({ req });
+      const accessToken = cookies.access_token;
+
+      if (!accessToken) {
+        res.status(401).json({ error: 'Access token not found' });
+        return;
+      }
+
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/tickets`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch tickets data');
+      }
+
+      const ticketsData = await response.json();
+
+      res.status(200).json(ticketsData);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}
