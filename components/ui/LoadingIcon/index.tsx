21711196622f355d64b6f2ce5bb1a5702c0e2c53
ahmetgursel <ahmetgursel@protonmail.com>
2023-08-16 03:29:21 +0300

    Add LoadingIcon component

---
+++ components/ui/LoadingIcon/index.tsx
@@ -0,0 +1,10 @@
+import { Center, Loader } from '@mantine/core';
+import React from 'react';
+
+const LoadingIcon = () => (
+  <Center style={{ minHeight: '100vh' }}>
+    <Loader />
+  </Center>
+);
+
+export default LoadingIcon;
