4185ef7e069447413286a18b68e29d4fa0cc14a0
ahmetgursel <ahmetgursel@protonmail.com>
2023-08-16 04:15:43 +0300

    Add swr package and create fetcher utility function

---
+++ utils/fetcher.ts
@@ -0,0 +1,11 @@
+const fetcher = async (...args: Parameters<typeof fetch>) => {
+  const response = await fetch(...args);
+
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+
+  return response.json();
+};
+
+export default fetcher;
