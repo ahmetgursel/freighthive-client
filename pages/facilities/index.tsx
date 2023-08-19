043636a9ce4f345aee65734f77936004d389114b
ahmetgursel <ahmetgursel@protonmail.com>
2023-08-19 04:13:39 +0300

    Refactor facilities page to use SWR for fetching data

--- pages/facilities/index.tsx
+++ pages/facilities/index.tsx
@@ -1,143 +1,27 @@
-import { ActionIcon, Button, Divider, Flex, Group, Modal, Table, Text } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
-import { modals } from '@mantine/modals';
-import { IconPlus, IconSettings, IconTrashOff } from '@tabler/icons';
 import router from 'next/router';
 import React from 'react';
+import useSWR from 'swr';
 import AppShellLayout from '../../components/AppShellLayout';
+import CustomTable from '../../components/ui/CustomTable';
+import HeaderGroup from '../../components/ui/HeaderGroup';
+import LoadingIcon from '../../components/ui/LoadingIcon';
 import useAuthentication from '../../hooks/useAuthentication';
+import fetcher from '../../utils/fetcher';
 
-interface FacilityRowData {
-  name: string;
-  address: string;
-  city: string;
-  country: string;
-}
-
-const FacilityRowData: FacilityRowData[] = [
-  {
-    name: 'Uzun Depo 1',
-    address: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac fringilla quam.',
-    city: 'Istanbul',
-    country: 'Turkey',
-  },
-  {
-    name: 'Geniş Depo 2',
-    address: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac fringilla quam.',
-    city: 'Ankara',
-    country: 'Turkey',
-  },
-  {
-    name: 'Köşeli Depo 3',
-    address: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac fringilla quam.',
-    city: 'Izmir',
-    country: 'Turkey',
-  },
-  {
-    name: 'Modern Depo 4',
-    address: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac fringilla quam.',
-    city: 'Bursa',
-    country: 'Turkey',
-  },
-  {
-    name: 'Küçük Depo 5',
-    address: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac fringilla quam.',
-    city: 'Adana',
-    country: 'Turkey',
-  },
-  {
-    name: 'Eski Depo 6',
-    address: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac fringilla quam.',
-    city: 'Antalya',
-    country: 'Turkey',
-  },
-  {
-    name: 'Modern Depo 7',
-    address: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac fringilla quam.',
-    city: 'Mersin',
-    country: 'Turkey',
-  },
-  {
-    name: 'Geniş Depo 8',
-    address: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac fringilla quam.',
-    city: 'Gaziantep',
-    country: 'Turkey',
-  },
-  {
-    name: 'Köşeli Depo 9',
-    address: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac fringilla quam.',
-    city: 'Konya',
-    country: 'Turkey',
-  },
-  {
-    name: 'Uzun Depo 10',
-    address: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac fringilla quam.',
-    city: 'Kayseri',
-    country: 'Turkey',
-  },
-  {
-    name: 'Uzun Depo 10',
-    address: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac fringilla quam.',
-    city: 'Kayseri',
-    country: 'Turkey',
-  },
-  {
-    name: 'Küçük Depo 11',
-    address: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac fringilla quam.',
-    city: 'Eskisehir',
-    country: 'Turkey',
-  },
-  {
-    name: 'Eski Depo 12',
-    address: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac fringilla quam.',
-    city: 'Trabzon',
-    country: 'Turkey',
-  },
-  {
-    name: 'Modern Depo 13',
-    address: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac fringilla quam.',
-    city: 'Isparta',
-    country: 'Turkey',
-  },
-  {
-    name: 'Geniş Depo 14',
-    address: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac fringilla quam.',
-    city: 'Kahramanmaras',
-    country: 'Turkey',
-  },
-  {
-    name: 'Köşeli Depo 15',
-    address: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac fringilla quam.',
-    city: 'Giresun',
-    country: 'Turkey',
-  },
+const columns = [
+  { key: 'name', title: 'İsim' },
+  { key: 'address', title: 'Adres' },
+  { key: 'city', title: 'Şehir' },
+  { key: 'country', title: 'Ülke' },
 ];
 
 const Facilities = () => {
   const authenticationData = useAuthentication();
-
-  const [openedNewFacilityButton, { open: openNewFacilityButton, close: closeNewFacilityButton }] =
-    useDisclosure(false);
-  const [openedUpdateIcon, { open: openUpdateIcon, close: closeUpdateIcon }] = useDisclosure(false);
-
-  const openDeleteModal = () =>
-    modals.openConfirmModal({
-      title: 'Bu depo kaydını silmek istediğinizden emin misiniz?',
-      centered: true,
-      children: (
-        <Text size="sm">
-          Bu işlem geri alınamaz. Bu depo kaydı ile ilgili tüm veriler silinecektir.
-        </Text>
-      ),
-      labels: { confirm: 'Depo Kaydı Sil', cancel: 'İptal' },
-      confirmProps: { color: 'red' },
-      onCancel: () => console.log('Cancel'),
-      onConfirm: () => console.log('Confirmed'),
-    });
+  const { data, error } = useSWR('/api/facilities', fetcher);
 
   if (authenticationData === null) {
-    // Eğer authenticationData henüz gelmemişse "Loading..." görüntüle
-    return <h1>Loading...</h1>;
+    // Eğer authenticationData henüz gelmemişse loading ikonunu görüntüle
+    return <LoadingIcon />;
   }
 
   if (authenticationData.isAuthenticated === false) {
@@ -146,88 +30,34 @@ const Facilities = () => {
     return null;
   }
 
-  const rows = FacilityRowData.map((data, index) => (
-    <tr key={index}>
-      <td>{data.name}</td>
-      <td>{data.address}</td>
-      <td>{data.city}</td>
-      <td>{data.country}</td>
-      <td>
-        <Flex gap="md">
-          <ActionIcon>
-            <IconSettings size="1rem" onClick={openUpdateIcon} />
-          </ActionIcon>
-          <ActionIcon color="red.7">
-            <IconTrashOff size="1rem" onClick={openDeleteModal} />
-          </ActionIcon>
-        </Flex>
-      </td>
-    </tr>
-  ));
+  if (!data && !error) {
+    // Veri henüz yüklenmemişse veya hata oluşmamışsa loading ikonunu görüntüle
+    return <LoadingIcon />;
+  }
+
+  if (error) {
+    // Hata durumunda hata mesajını görüntüle
+    // TODO: error page oluştur
+    return <div>Hata oluştu: {error.message}</div>;
+  }
 
   return (
     <AppShellLayout>
-      <Flex gap="md" justify="space-between" align="center" direction="row" wrap="wrap" px="md">
-        <Text fz="xl" fw={700}>
-          Depo Listesi
-        </Text>
+      <HeaderGroup
+        modalTitle="Yeni Depo Ekle"
+        title="Depo Listesi"
+        addButtonTitle="Yeni Depo Ekle"
+      />
 
-        <Modal
-          opened={openedNewFacilityButton}
-          onClose={closeNewFacilityButton}
-          title="Yeni Depo Kayıt Ekle"
-          centered
-        >
-          {/* yeni cari kaydı ekleme modal buraya */}
-        </Modal>
-
-        <Group position="center">
-          <Button
-            size="md"
-            radius="lg"
-            compact
-            variant="light"
-            rightIcon={<IconPlus size="1rem" />}
-            onClick={openNewFacilityButton}
-            loading={openedNewFacilityButton}
-            loaderPosition="right"
-          >
-            Yeni Depo Kayıt Ekle
-          </Button>
-        </Group>
-      </Flex>
-
-      <Divider my="sm" size={1} />
-
-      <Modal
-        opened={openedUpdateIcon}
-        onClose={closeUpdateIcon}
-        title="Depo Kayıt Güncelle"
-        centered
-      >
-        {/* cari kayıt güncelleme modal buraya */}
-      </Modal>
-
-      <Table
-        verticalSpacing="md"
-        horizontalSpacing="md"
-        fontSize="lg"
-        my="md"
-        mx="md"
-        striped
-        highlightOnHover
-      >
-        <thead>
-          <tr>
-            <th style={{ width: '25%' }}>İsim</th>
-            <th>Adres</th>
-            <th style={{ width: '10%' }}>Şehir</th>
-            <th style={{ width: '10%' }}>Ülke</th>
-            <th />
-          </tr>
-        </thead>
-        <tbody>{rows}</tbody>
-      </Table>
+      <CustomTable
+        columns={columns}
+        data={data}
+        updateModalTitle="Depo Kaydını Güncelle"
+        deleteModalTitle="Bu depo kaydını silmek istediğinizden emin misiniz?"
+        deleteModalText="Bu işlem geri alınamaz. Bu depo kaydıyla ilgili tüm veriler silinecektir."
+        deleteModalConfirmButtonLabel="Depo Kaydını Sil"
+        deleteModalCancelButtonLabel="İptal"
+      />
     </AppShellLayout>
   );
 };
